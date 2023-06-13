a597a176fcec1dd7a320cdc21c602f20bfd98056
alaasouabni <alaa.souabni@gmail.com>
2023-06-13 23:24:59 +0200

    update index

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.all(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
   res.header(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
   );
   res.header('Access-Control-Expose-Headers', 'Set-Cookie');
   next();
